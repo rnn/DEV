5cc3cd1c9a7476e4aa006d77ca948ea52c0905aa
rnn <rania.guermazi@gmail.com>
2013-08-22 18:40:08 +0100

    initial setup

---
+++ 1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Document sans titre</title>
+</head>
+
+<body>
+<pre>&lt;HTML&gt;
+
+   &lt;BODY&gt;
+      &lt;?php      echo &quot;Hello world!&quot;;    ?&gt;
+   &lt;/BODY&gt;
+&lt;/HTML&gt;</pre>
+</body>
+</html>
